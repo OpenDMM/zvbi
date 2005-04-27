8a600b61efc1f74acebe568a09cb4f54b86e2e27
mschimek <mschimek>
2005-04-27 10:54:11 +0000

    vbi_printable() undefined.

--- test/caption.c
+++ test/caption.c
@@ -40,6 +40,20 @@
 
 #include "src/libzvbi.h"
 
+vbi_inline int
+vbi_printable			(int			c)
+{
+	if (c < 0)
+		return '?';
+
+	c &= 0x7F;
+
+	if (c < 0x20 || c >= 0x7F)
+		return '.';
+
+	return c;
+}
+
 vbi_decoder *		vbi;
 vbi_pgno		pgno = -1;
 
