1f24a45adcd64bab32e8a798e01c7adc5f9b996a
mschimek <mschimek>
2008-02-24 14:18:17 +0000

    test_malloc: Muffle compiler warnings.

--- test/test-common.cc
+++ test/test-common.cc
@@ -117,6 +117,7 @@ xmemdup				(const void *		src,
 static unsigned int		malloc_count;
 static unsigned int		malloc_fail_cycle;
 
+#if 3 == VBI_VERSION_MINOR
 static void *
 my_malloc			(size_t			n_bytes)
 {
@@ -125,6 +126,7 @@ my_malloc			(size_t			n_bytes)
 	else
 		return malloc (n_bytes);
 }
+#endif
 
 void
 test_malloc			(void			(* function)(void),
@@ -140,6 +142,9 @@ test_malloc			(void			(* function)(void),
 	}
 
 	vbi_malloc = malloc;
+#else
+	function = function; /* unused */
+	n_cycles = n_cycles;
 #endif
 }
 
