cb3d02438f19c4b2365dbc5a797c661c13d669db
mschimek <mschimek>
2009-03-04 21:47:59 +0000

    vbi_event_enable, vbi_decode: The function now supports VBI_EVENT_LOCAL_TIME and VBI_EVENT_PROG_ID.

--- src/vbi.c
+++ src/vbi.c
@@ -160,6 +160,8 @@ vbi_event_enable(vbi_decoder *vbi, int mask)
 			vbi->aspect_source = 0;
 		}
 	}
+	if (activate & VBI_EVENT_PROG_ID)
+		CLEAR (vbi->vps_pid);
 
 	vbi->event_mask = mask;
 }
@@ -437,11 +439,15 @@ vbi_decode(vbi_decoder *vbi, vbi_sliced *sliced, int lines, double time)
 
 	  if (vbi->event_mask & (VBI_EVENT_TTX_PAGE |
 				 VBI_EVENT_NETWORK |
-				 VBI_EVENT_NETWORK_ID))
+				 VBI_EVENT_NETWORK_ID |
+				 VBI_EVENT_LOCAL_TIME |
+				 VBI_EVENT_PROG_ID))
 		  vbi_teletext_desync(vbi);
 	  if (vbi->event_mask & (VBI_EVENT_CAPTION |
 				 VBI_EVENT_NETWORK |
-				 VBI_EVENT_NETWORK_ID))
+				 VBI_EVENT_NETWORK_ID |
+				 VBI_EVENT_LOCAL_TIME |
+				 VBI_EVENT_PROG_ID))
 		  vbi_caption_desync(vbi);
 	} else {
 		pthread_mutex_lock(&vbi->chswcd_mutex);
